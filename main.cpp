b1fbf62b1fd92f3b09ee57d9900738409c8c7d3a
Gabriel França <32433878+GabrielFOliveira@users.noreply.github.com>
2021-10-14 22:45:09 -0300

    iniciando desenvolvimento do CMD

--- main.cpp
+++ main.cpp
@@ -4,19 +4,42 @@
 
 #include "tabelahash.h"
 
-int main(){
+int main(int argc, char *argv[]){
+  // A variavel "teste" e usada para debug
+  // Comente a linha 12 para rodar seu teste contido no if abaixo
+  // Se não cairá no CMD
+  int teste = 1;
+  teste=0;
+  if (teste == 1){
+    TabelaHash tabela;
+    new_hash(&tabela);
+    Registro a;
+    a.reg="testestring";
+    add_hash(&tabela,a);
+    print_tabela_hash(&tabela);
+  }
+  else{
+    if (argc == 1 || strcmp(argv[2],"--help")==0){
+        printf("simpledb [cmd]\n"
+        "  --insert=<sort-key,value> \n      Insere um objeto no banco de dados.\n"
+        "  --remove=<key>\n      Remove do banco de dados o objeto identificado pela chave key.\n"
+        "  --search=<key>\n      Busca no banco de dados objeto identificado pela chave key.\n"
+        "  --update=<key,sort-key,value>\n      Atualiza o objeto que e identificado pela chave key. \n"
+        "  --list=<expr>\n      Lista em ordem crescente todos os objetos que possam uma chave de ordenacao que atenda aos criterios especificados pela expressao 'expr'.\n"
+        "  --reverse-list=<expr>\n      Lista em ordem decrescente  todos os objetos que possam uma chave de ordenacao que atenda aos criterios especificados pela expressao 'expr'.\n"
+        "  * A expressao 'expr' deve aceitar qualquer operacao logica simples envolvendo a chave:\n"
+        "      key>X: objetos que possuem chave de ordenacao maior que X.\n"
+        "      key<X: objetos que possuem chave de ordenacao menor que X.\n"
+        "      key=X: objetos que possuem chave ordenacao igual a X.\n"
+        "      key>=X: objetos que possuem chave de ordenacao maior ou igual que X.\n"
+        "      key<=X: objetos que possuem chave de ordenacao menor ou igual que X.\n"
+        "  --compress=[huffman|lzw]\n      Compacta os registros do banco de dados usando o algoritmo de Codificacao de Huffman ou o Algoritmo de Compressao LZW. \n"
+        "  --decompress=[huffman|lzw]\n      Descompacta os registros do banco de dados usando o algoritmo de Codificacao de Huffman ou o Algoritmo de Compressao LZW. \n"
+        "");
 
-TabelaHash tabela;
+    }
+  }
 
-new_hash(&tabela);
-
-Registro a;
-
-a.reg="testestring";
-
-add_hash(&tabela,a);
-
-print_tabela_hash(&tabela);
 
 return 0;
 }
